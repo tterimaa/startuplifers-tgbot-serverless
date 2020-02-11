6dafa20e9f1c82d03cb027e63c045d9456828055
tterimaa <tterimaa@gmail.com>
2020-02-11 13:04:46 +0200

    New jobs filtering bug fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const isTest = process.env.NODE_ENV === 'test';
 const apiUrl = 'https://api.lever.co/v0/postings/startuplifers?mode=json';
-const fileName = isTest ? 'testJobs.json': 'jobs.json';
+const fileName = 'jobs.json';
 const channelName = '@testikanava123';
 
 export async function main(event, context) {
@@ -15,14 +15,19 @@ export async function main(event, context) {
   } catch(err) {
     return { statusCode: 500, body: `Error while getting stored jobs: ${err}` };
   }
-
   let apiJobs = await getApiJobs(apiUrl);
-  let newJobs = apiJobs.filter(jobApi => !storedJobs.map(jobStored => jobStored.id).includes(jobApi.id));
-
+  let storedIds = storedJobs.map(job => job.id);
+  let newJobs = [];
+  for(let job of apiJobs) {
+    if(storedIds.includes(job.id)) newJobs.push(job);
+  };
+  console.log('api jobs' + apiJobs.length);
+  console.log('new jobs' + newJobs.length);
   if(isTest) return newJobs;
 
   if(newJobs) {
     let promises = newJobs.map(job => bot.sendMessage(channelName, `${job.hostedUrl}\n\n${job.text}`));
+    console.log(promises.length);
     await Promise.all(promises);
     try {
        await storeJobs('startuplifersbucket', 'jobs.json', apiJobs);
