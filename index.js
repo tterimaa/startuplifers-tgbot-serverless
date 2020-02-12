96ba469286a186287066b3f810de1864085b3edb
tterimaa <tterimaa@gmail.com>
2020-02-12 12:55:48 +0200

    Tests

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
-import { getStoredJobs, storeJobs, getApiJobs } from './services';
-import bot from './bot';
-import dotenv from 'dotenv';
+import { getStoredJobs, storeJobs, getApiJobs } from "./services";
+import bot from "./bot";
+import dotenv from "dotenv";
 dotenv.config();
 
-const isTest = process.env.NODE_ENV === 'test';
-const apiUrl = 'https://api.lever.co/v0/postings/startuplifers?mode=json';
-const fileNameToFetch = isTest ? 'testJobs.json' : 'jobs.json';
-const fileNameToUpload = 'jobs.json';
-const bucketName = 'startuplifersbucket';
-const channelName = '@testikanava123';
+const apiUrl = "https://api.lever.co/v0/postings/startuplifers?mode=json";
+const fileName = "jobs.json";
+const bucketName = "startuplifersbucket";
+const channelName = "@testikanava123";
 
 export async function main() {
   let storedJobs;
   try {
-    storedJobs = await getStoredJobs(bucketName, fileNameToFetch);
-  } catch(err) {
+    storedJobs = await getStoredJobs(bucketName, fileName);
+  } catch (err) {
     return { statusCode: 500, body: `Error while getting stored jobs: ${err}` };
   }
   const apiJobs = await getApiJobs(apiUrl);
-  const newJobs = apiJobs.filter(apiJob => !storedJobs.map(job => job.id).includes(apiJob.id));
-  console.log('api jobs: ' + apiJobs.length);
-  console.log('new jobs: ' + newJobs.length);
-  if(isTest) return newJobs;
+  const newJobs = apiJobs.filter(
+    apiJob => !storedJobs.map(job => job.id).includes(apiJob.id)
+  );
 
-  if(newJobs) {
-    const promises = newJobs.map(job => bot.sendMessage(channelName, `${job.hostedUrl}\n\n${job.text}`));
+  if (newJobs) {
+    const promises = newJobs.map(job =>
+      bot.sendMessage(channelName, `${job.hostedUrl}\n\n${job.text}`)
+    );
     await Promise.all(promises);
     try {
-       await storeJobs(bucketName, fileNameToUpload, apiJobs);
-    } catch(err) {
+      await storeJobs(bucketName, fileName, apiJobs);
+    } catch (err) {
       return { statusCode: 500, body: `Error while storing new jobs: ${err}` };
     }
   }
 
-  return { statusCode: 200, body: 'Function invoked succesfully' };
+  return {
+    statusCode: 200,
+    body: {
+      message: "Function invoked succesfully",
+      jobsSent: newJobs
+    }
+  };
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,48 @@
-import { main } from '../index';
-import axios from 'axios'
+import { main } from "../index";
+import { storeJobs, getApiJobs } from "../services";
 
-test('New jobs equals jobs in api when store is empty', async () => {
-  const jobs = await main();
-  const api = await axios.get('https://api.lever.co/v0/postings/startuplifers?mode=json');
-  expect(api.data.length).toEqual(jobs.length);
+const bucketName = "startuplifersbucket";
+
+let apiJobs;
+
+beforeEach(async () => {
+  apiJobs = await getApiJobs(
+    "https://api.lever.co/v0/postings/startuplifers?mode=json"
+  );
 });
+
+describe("Test api", () => {
+  test("Jobs api returns something", () => {
+    expect(apiJobs.length).toBeGreaterThan(0);
+  });
+});
+
+describe("Test index function", () => {
+  test("Sending all of the jobs when store is empty", async () => {
+    const emptyJobs = [{}];
+    await storeJobs(bucketName, "jobs.json", emptyJobs);
+    const res = await main();
+    expect(res.body.jobsSent.length).toEqual(apiJobs.length);
+  }, 30000);
+  
+  test("Sending none of the jobs when store is same as api", async () => {
+    await storeJobs(bucketName, "jobs.json", apiJobs);
+    const res = await main();
+    expect(res.body.jobsSent.length).toEqual(0);
+  }, 30000);
+  
+  test("The first job is new when it's missing from store", async () => {
+    const removedJob = apiJobs.shift();
+    await storeJobs(bucketName, "jobs.json", apiJobs);
+    const res = await main();
+    expect(res.body.jobsSent[0].id).toEqual(removedJob.id);
+  }, 30000);
+  
+  test("Sent jobs array length is 1 when the difference between api and store is 1", async () => {
+    apiJobs.shift();
+    await storeJobs(bucketName, "jobs.json", apiJobs);
+    const res = await main();
+    expect(res.body.jobsSent.length).toEqual(1);
+  }, 30000);
+  
+})
